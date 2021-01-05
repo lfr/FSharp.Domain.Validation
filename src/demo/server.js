9ea33ceaa57d6d545366e5c23bbe337f1664d03d
Luis Ferrao <6348118+lferrao@users.noreply.github.com>
2021-01-05 15:14:21 +0100

    testing automated deployment

--- src/demo/server.js
+++ src/demo/server.js
@@ -4,8 +4,9 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT);
+app.set('host', '0.0.0.0');
 
-var server = app.listen(app.get('port'), function () {
+var server = app.listen(app.get('port'), app.get('host'), function () {
     console.log('listening on port ', server.address().port);
 });
