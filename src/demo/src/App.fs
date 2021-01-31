637048180b67f60b9ec7674f0832212996777e7d
Luis Ferrao <6348118+lferrao@users.noreply.github.com>
2021-01-31 18:49:55 +0100

    demo update

--- src/demo/src/App.fs
+++ src/demo/src/App.fs
@@ -5,24 +5,24 @@ open type FSharp.ValidationBlocks.Fable.Block<string, TextError>
 open Browser.Dom
 
 // Get bindings for the text area and 3 result contains
-let box = document.querySelector("#inp") :?> Browser.Types.HTMLTextAreaElement
+let inp = document.querySelector("#inp") :?> Browser.Types.HTMLTextAreaElement
 let result1 = document.querySelector("#resultBox1") :?> Browser.Types.HTMLSpanElement
 let result2 = document.querySelector("#resultBox2") :?> Browser.Types.HTMLSpanElement
 let result3 = document.querySelector("#resultBox3") :?> Browser.Types.HTMLSpanElement
 
 // Register our listener
-box.oninput <- fun _ ->
+inp.oninput <- fun _ ->
 
     result1.textContent <-
-        validate<Text> box.value
+        validate<Text> inp.value
         |> Result.toText
 
     result2.textContent <-
-        validate<FreeText> box.value
+        validate<FreeText> inp.value
         |> Result.toText
 
     result3.textContent <-
-        validate<Integer> box.value
+        validate<Integer> inp.value
         |> Result.toText 
 
 
